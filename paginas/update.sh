eba3c0699fbcfa91b92c56ab80f5a04a7ea27b22
maicon-concurso <pvhservi@gmail.com>
2024-07-31 18:28:19 -0400

    atualização da página ata

---
+++ paginas/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Adicionar todos os arquivos modificados
+git add .
+
+# Fazer um commit com uma mensagem
+git commit -m "Atualizações automáticas"
+
+# Enviar as mudanças para o repositório remoto
+git push
